a22f0888265a2f5fea11983aa5f4325e4dd03d1f
J. Grimberg <github@joelgrimberg.nl>
2024-09-25 16:02:33 +0200

    populate the uses page

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,36 +1,38 @@
 describe('template spec', () => {
-    it('Should show the Title', () => {
-        cy.visit('/')
-        cy.contains('Joël Grimberg')
-    })
+  beforeEach(() => {
+    cy.visit('/')
+  })
 
-    it('Should load a blog post', () => {
-        cy.visit('/blog/first-post')
-        cy.contains('First post')
-    })
+  it('Should show the Title', () => {
+    cy.contains('Joël Grimberg')
+  })
 
-    it('Should be able to use the Social Links inside the header', () => {
-        cy.visit('/')
+  it('Should load a blog post', () => {
+    cy.contains('Blog').click()
+    cy.contains('First post').click()
+    cy.url().should('include', '/blog/first-post')
+    cy.contains('First post')
+  })
 
-        cy.get('[test-id=twitter-link]')
-            .should('be.visible')
-            .and('have.attr', 'href', 'https://twitter.com/joelgrimberg')
+  it('Should be able to use the Social Links inside the header', () => {
+    cy.get('[test-id=twitter-link]')
+      .should('be.visible')
+      .and('have.attr', 'href', 'https://twitter.com/joelgrimberg')
 
-        cy.get('[test-id=github-link]')
-            .should('be.visible')
-            .and('have.attr', 'href', 'https://github.com/joelgrimberg')
-    })
+    cy.get('[test-id=github-link]')
+      .should('be.visible')
+      .and('have.attr', 'href', 'https://github.com/joelgrimberg')
+  })
 
-    it('Should be able to use the Social Links inside the footer', () => {
-        cy.visit('/')
-        cy.get('[test-id=twitter-link-footer]')
-            .scrollIntoView()
-            .should('be.visible')
-            .and('have.attr', 'href', 'https://twitter.com/joelgrimberg')
+  it('Should be able to use the Social Links inside the footer', () => {
+    cy.get('[test-id=twitter-link-footer]')
+      .scrollIntoView()
+      .should('be.visible')
+      .and('have.attr', 'href', 'https://twitter.com/joelgrimberg')
 
-        cy.get('[test-id=github-link-footer]')
-            .scrollIntoView() 
-            .should('be.visible')
-            .and('have.attr', 'href', 'https://github.com/joelgrimberg')
-    })
+    cy.get('[test-id=github-link-footer]')
+      .scrollIntoView()
+      .should('be.visible')
+      .and('have.attr', 'href', 'https://github.com/joelgrimberg')
+  })
 })
